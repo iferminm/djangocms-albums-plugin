6bb6937a5b4a9967b6d8e78fea5163e958332a9d
Israel Fermín Montilla <ferminster@gmail.com>
2016-07-29 15:04:19 +0400

    bump up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='djangocms-albums-plugin',
-    version='0.0.2',
+    version='0.0.3',
     author=u'Israel Fermín Montilla',
     author_email='iferminm@gmail.com',
     packages=find_packages(),
